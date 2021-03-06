cacf4525d7e411a0e33c565efedb2fe0635023aa
enderweb <74277937+enderweb@users.noreply.github.com>
2021-03-06 12:04:59 -0500

    made it work with python3

--- dogYearsPython/dogYearsPython.py
+++ dogYearsPython/dogYearsPython.py
@@ -1,4 +1,4 @@
-print "How old is your dog?"
+print ("How old is your dog?")
 dogAge = input()
 inHuman = dogAge * 7
-print "The dog is " + str(inHuman) + " years old"
+print ("The dog is " + str(inHuman) + " years old")
