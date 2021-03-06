ba6f694aa641cc1c39fda009164a9b3fe24b4409
enderweb <74277937+enderweb@users.noreply.github.com>
2021-03-06 10:39:52 -0500

    file

---
+++ dogYears/dogYears.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+int dogAge = 0;
+cout << "How old is the dog" << endl;
+cin >> dogAge;
+cout << "The dog is " << dogAge * 7 << " years old." << endl;
+}
